80a50e3061cceefd87ab739457b202d27ee855f9
Michael Tint <36082998+mtint@users.noreply.github.com>
2021-11-09 22:11:15 +0000

    first project

---
+++ Python/Week 1/Day 1/1-areaoftriangel.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov  9 22:00:14 2021
+
+@author: mtint
+"""
+
+def areatriangle(b,h):
+    area=0.5*b*h
+    print("The area of a Triangle is", area)
+
+
+areatriangle(12,9);
+
